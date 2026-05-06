c6a3e1f4c22fd115ad7e42dd2930cd85a939654d
szn225 <szn225ann@gmail.com>
2026-05-06 17:03:30 +0800

    fix: simplify next config

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingExcludes: {
-    '*': [
-      'node_modules/@swc/core-linux-x64-gnu',
-      'node_modules/@swc/core-linux-x64-musl',
-      'node_modules/esbuild',
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
